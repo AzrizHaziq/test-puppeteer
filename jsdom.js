f483b7d8aebf3c11d8ab1f063a7519eac0d416d6
azrizhaziq <azrizhaziq@gmail.com>
2021-03-25 18:39:18 +0800

    Try jsdom

---
+++ jsdom.js
@@ -0,0 +1,19 @@
+import { JSDOM } from 'jsdom'
+import fetch from 'isomorphic-fetch'
+;(async () => {
+  const res = await fetch(
+    'https://www.bursamalaysia.com/market_information/shariah_compliant_equities_prices?top_stock=top_active&per_page=50&page=1'
+  )
+  const dom = await res.text()
+
+  const { document } = new JSDOM(dom).window
+
+  const pagination = Array.from(document.querySelectorAll('.pagination li [data-val]'))
+    .map(i => i.textContent)
+    .filter(Boolean)
+    .map(parseFloat)
+
+  const maxPageNumber = Math.max(...pagination)
+
+  console.log(maxPageNumber)
+})()
