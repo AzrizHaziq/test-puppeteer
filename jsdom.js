18ff1108e7083b42779b7549d22d5495a6fa7871
azrizhaziq <azrizhaziq@gmail.com>
2021-03-25 19:18:42 +0800

    update docker

--- jsdom.js
+++ jsdom.js
@@ -5,8 +5,10 @@ import fetch from 'isomorphic-fetch'
     'https://www.bursamalaysia.com/market_information/shariah_compliant_equities_prices?top_stock=top_active&per_page=50&page=1'
   )
   const dom = await res.text()
-
-  const { document } = new JSDOM(dom).window
+  const { document } = new JSDOM(dom, {
+    url: 'https://www.bursamalaysia.com',
+    referrer: 'https://www.bursamalaysia.com',
+  }).window
 
   const pagination = Array.from(document.querySelectorAll('.pagination li [data-val]'))
     .map(i => i.textContent)
@@ -14,6 +16,19 @@ import fetch from 'isomorphic-fetch'
     .map(parseFloat)
 
   const maxPageNumber = Math.max(...pagination)
+  console.log(maxPageNumber)
+})()
+;(async () => {
+  const dom = await JSDOM.fromURL(
+    'https://www.bursamalaysia.com/market_information/shariah_compliant_equities_prices?top_stock=top_active&per_page=50&page=1'
+  )
 
+  const { document } = new JSDOM(dom.serialize()).window
+  const pagination = Array.from(document.querySelectorAll('.pagination li [data-val]'))
+    .map(i => i.textContent)
+    .filter(Boolean)
+    .map(parseFloat)
+
+  const maxPageNumber = Math.max(...pagination)
   console.log(maxPageNumber)
 })()
