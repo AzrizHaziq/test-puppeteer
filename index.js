e63c05783e3a1c4c250aac18d02071202e1dc7ef
azrizhaziq <azrizhaziq@gmail.com>
2021-04-20 15:41:46 +0800

    try again

--- index.js
+++ index.js
@@ -1,42 +1,47 @@
 import puppeteer from 'puppeteer'
+;(async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
 
-(async() => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage()
+  console.log('before goto')
 
-    console.log('before goto')
-    await page.goto('https://www.bursamalaysia.com/market_information/shariah_compliant_equities_prices?top_stock=top_active&per_page=50&page=1')
+  await page.goto(
+    'https://www.bursamalaysia.com/market_information/shariah_compliant_equities_prices?top_stock=top_active&per_page=50&page=1',
+    {
+      waitUntil: 'networkidle0',
+    }
+  )
 
-    console.log('after goto')
+  console.log('after goto')
 
-    const title = await page.evaluate(p => {
-        return document.title
-    })
+  const title = await page.evaluate(p => {
+    return document.title
+  })
 
-    console.log('title: ', title);
+  console.log('title: ', title)
 
-    // await page.waitForSelector('.pagination li [data-val]')
-    // const pagination = await page.$$('.pagination li [data-val]')
+  // await page.waitForSelector('.pagination li [data-val]')
+  // const pagination = await page.$$('.pagination li [data-val]')
 
-    // let temp = []
-    // for await (let page of pagination) {
-    //     const textContent = await page.evaluate(el => el.textContent)
-    //     temp.push(textContent)
-    // }
+  // let temp = []
+  // for await (let page of pagination) {
+  //     const textContent = await page.evaluate(el => el.textContent)
+  //     temp.push(textContent)
+  // }
 
-    // const maxPages = Math.max(...temp.filter(Boolean).map(parseFloat))
-    // console.log(maxPages)
+  // const maxPages = Math.max(...temp.filter(Boolean).map(parseFloat))
+  // console.log(maxPages)
 
-    // const def = await page.evaluate(() => {
-    //   document.querySelector('#DataTables_Table_0_paginate > ul.pagination').scrollIntoView();
+  // const def = await page.evaluate(() => {
+  //   document.querySelector('#DataTables_Table_0_paginate > ul.pagination').scrollIntoView();
 
-    // return Array.from(document.querySelectorAll('#DataTables_Table_0_paginate > ul.pagination'))
-    //   .map(i => i.textContent)
-    //   .filter(Boolean)
-    //   .join(', ')
-    // return document.title;
-    // return document.querySelector("#navbarDropdown").textContent
-    // })
+  // return Array.from(document.querySelectorAll('#DataTables_Table_0_paginate > ul.pagination'))
+  //   .map(i => i.textContent)
+  //   .filter(Boolean)
+  //   .join(', ')
+  // return document.title;
+  // return document.querySelector("#navbarDropdown").textContent
+  // })
 
   // console.log('bursamalaysia', def)
 
